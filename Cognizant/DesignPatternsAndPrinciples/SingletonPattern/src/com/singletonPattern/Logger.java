8e0ecfd810fa9934ee541e7c873072ff76fbb632
dhivya-dharshini65 <727723eucs055@skcet.ac.in>
2026-06-22 15:04:30 +0530

    SingletonPattern

---
+++ Cognizant/DesignPatternsAndPrinciples/SingletonPattern/src/com/singletonPattern/Logger.java
@@ -0,0 +1,5 @@
+package com.singletonPattern;
+
+public class Logger {
+
+}
---
+++ Cognizant/DesignPatternsAndPrinciples/SingletonPattern/src/com/singletonPattern/SingletonTest.java
@@ -0,0 +1,10 @@
+package com.singletonPattern;
+
+public class SingletonTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
